6cb1096073f08ff8eb4f7da236c26c1be9124264
Alongkod113 <80100652+Alongkod113@users.noreply.github.com>
2021-12-20 20:42:53 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="สอบ.css">
+</head>
+
+<body>
+        <div class="content">
+            <div class="from-group mb-3 ">
+                <span class="from-group-text">Username</span><br>
+                <input type="text" class="form-control" placeholder="" aria-label="Latitude" aria-describedby="Latitude"
+                    id="cj">
+            </div>
+            <div class="from-group mb-3">
+                <span class="from-group-text">password</span><br>
+                <input type="text" class="form-control" placeholder="" aria-label="Longitude"
+                    aria-describedby="Longitude" id="1234">
+            </div><br>
+            <div>
+                <button class="button" id="btnSearch">Login</button>
+                <link rel="stylesheet" href="">
+                <div><font color="#FF0000">error cannot login</font></div>
+                <br>
+            </div>
+        </div>
+</body>
+
+</html>
